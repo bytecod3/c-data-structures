330a56cb86b0f96750f63f003fe3e0f1c46f21a4
bytecod3 <emwiti658@gmail.com>
2021-06-27 01:36:26 +0300

    implement insertion functions

--- linked_list.c
+++ linked_list.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int choice, count;
+int choice, count, position, i = 1;
 struct node{
     int data;
     struct node* next;
 };
 
-struct node* head, *newnode, *temp;
+struct node* head, *new_node, *temp;
 
 // initially head points to 0
 head = 0;
@@ -16,21 +16,21 @@ head = 0;
 void init_list(){
     while(choice){
         // create a new node
-        newnode = (struct node *) malloc(sizeof(struct node*));
+        new_node = (struct node *) malloc(sizeof(struct node));
 
         printf("Enter data: ");
-        scanf("%d", &newnode->data);
+        scanf("%d", &new_node->data);
 
-        // the newnode's address part now contains a zero
-        newnode->next = 0;
+        // the new_node's address part now contains a zero
+        new_node->next = 0;
 
         // update head address to the new node
         if (head == 0){
-            head = newnode;
-            temp = newnode;
+            head = new_node;
+            temp = new_node;
         }else{
-            temp->next = newnode;
-            temp = newnode; // move the temp to the new node
+            temp->next = new_node;
+            temp = new_node; // move the temp to the new node
         }
 
         // ask user for input
@@ -59,7 +59,53 @@ int list_count(){
     return count;
 }
 
+void insert_at_beginning(){
+    new_node = (struct node *) malloc(sizeof(struct node));
+    printf("Enter data to insert: ");
+    scanf("%d", &new_node->data);
 
+    // update pointers
+    if(head == 0){
+        new_node->next = head;
+        head = new_node;
+    }
+}
 
+void insert_at_end(){
+    // create a new node
+    new_node = (struct node *) malloc(sizeof(struct node));
+    printf("Enter data to insert: ");
+    scanf("%d", &new_node->data);
+
+    while (temp->next != 0){
+        temp = temp->next;
+    }
+
+    temp->next = new_node;
+    
+}
+
+void insert_after_position(int position){
+    // call count function to get the number of elements in the list
+    int num = list_count();
+
+    if(num > position){
+        printf("Invalid position."); // list full or negative
+    }else{
+        while(i < position){
+            temp = temp->next; // update temp
+            i++;
+        }
+
+        new_node = (struct node *) malloc(sizeof(struct node));
+
+        printf("Enter data:"); // ask for data from user
+        scanf("%d", &new_node->data);
+
+        new_node->next = temp->next;
+        temp->next = new_node;
+        
+    }
+}
 
 
